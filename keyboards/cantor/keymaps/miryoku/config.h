784f4f7fd94015b173c650be541d5e96d43939d8
Simon Renhult <simon.renhult@hotmail.se>
2025-06-28 15:52:45 +0200

    Miryoku: Simplify layout specification, added game layer, and fixed mouse keys

--- keyboards/cantor/keymaps/miryoku/config.h
+++ keyboards/cantor/keymaps/miryoku/config.h
@@ -1,13 +1,13 @@
 #define ENABLE_COMPILE_KEYCODE
 
-#define TAPPING_TERM             200
-#define QUICK_TAP_TERM           0
+#define TAPPING_TERM               200
+#define QUICK_TAP_TERM             0
 
 #define MOUSEKEY_DELAY             0
 #define MOUSEKEY_INTERVAL          16
-#define MOUSEKEY_MAX_SPEED         1
-#define MOUSEKEY_TIME_TO_MAX       16
+#define MOUSEKEY_MAX_SPEED         2
+#define MOUSEKEY_TIME_TO_MAX       32
 #define MOUSEKEY_WHEEL_DELAY       0
 #define MOUSEKEY_WHEEL_INTERVAL    80
 #define MOUSEKEY_WHEEL_MAX_SPEED   2
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 8
--- keyboards/cantor/keymaps/miryoku/keymap.c
+++ keyboards/cantor/keymaps/miryoku/keymap.c
@@ -12,7 +12,22 @@ enum {
     L_Mouse,
     L_Nav,
     L_Media,
+    L_Game,
+    L_GameSnd,
 };
+
+// NOTE(simon): Make it easier to specify layers.
+#define split_layout(                                                                       \
+        k00, k01, k02, k03, k04,                 k05, k06, k07, k08, k09,                   \
+        k10, k11, k12, k13, k14,                 k15, k16, k17, k18, k19,                   \
+        k20, k21, k22, k23, k24,                 k25, k26, k27, k28, k29,                   \
+                        t0,  t1,  t2,        t3,  t4,  t5                                   \
+    ) LAYOUT_split_3x6_3(                                                                   \
+        XXXXXXX, k00, k01, k02, k03, k04,                 k05, k06, k07, k08, k09, XXXXXXX, \
+        XXXXXXX, k10, k11, k12, k13, k14,                 k15, k16, k17, k18, k19, XXXXXXX, \
+        XXXXXXX, k20, k21, k22, k23, k24,                 k25, k26, k27, k28, k29, XXXXXXX, \
+                                 t0,  t1,  t2,        t3,  t4,  t5                          \
+    )
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* Base
@@ -29,16 +44,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤Tab│   │Ent├───┘
       *                   └───┘   └───┘
       */
-    [L_Base] = LAYOUT_split_3x6_3(
-        XXXXXXX,         KC_Q,         KC_W,         KC_E,              KC_R,              KC_T,                                                               KC_Y,              KC_U,         KC_I,         KC_O,             KC_P, XXXXXXX,
-        XXXXXXX, LSFT_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D),      LCTL_T(KC_F),              KC_G,                                                               KC_H,      RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L),  RSFT_T(KC_SCLN), XXXXXXX,
-        XXXXXXX,         KC_Z,         KC_X,         KC_C,              KC_V,              KC_B,                                                               KC_N,              KC_M,      KC_COMM,       KC_DOT,          KC_SLSH, XXXXXXX,
-                                                           LT(L_Nav, KC_ESC), LT(L_Num, KC_SPC), LT(L_Mouse, KC_TAB),          LT(L_Media, KC_ENT), RSFT_T(KC_BSPC), LT(L_Fun, KC_DEL)
+    [L_Base] = split_layout(
+                KC_Q,         KC_W,         KC_E,              KC_R,              KC_T,                                                               KC_Y,              KC_U,         KC_I,         KC_O,             KC_P,
+        LSFT_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D),      LCTL_T(KC_F),              KC_G,                                                               KC_H,      RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L),  RSFT_T(KC_SCLN),
+                KC_Z,         KC_X,         KC_C,              KC_V,              KC_B,                                                               KC_N,              KC_M,      KC_COMM,       KC_DOT,          KC_SLSH,
+                                                  LT(L_Nav, KC_ESC), LT(L_Num, KC_SPC), LT(L_Mouse, KC_TAB),          LT(L_Media, KC_ENT), RSFT_T(KC_BSPC), LT(L_Fun, KC_DEL)
     ),
 
      /* Numbers & symbols
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │MKE│   │   │   │   │       │[{ │7& │8* │9( │-_ │
+      * │MKE│Gam│   │   │   │       │[{ │7& │8* │9( │-_ │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │Sft│GUI│Alt│Ctl│   │       │}] │4$ │5% │6^ │=+ │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
@@ -50,16 +65,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   │   │   ├───┘
       *                   └───┘   └───┘
       */
-    [L_Num] = LAYOUT_split_3x6_3(
-        XXXXXXX, QK_MAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_LBRC,   KC_7, KC_8, KC_9, KC_MINS, XXXXXXX,
-        XXXXXXX, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX,                          KC_RBRC,   KC_4, KC_5, KC_6,  KC_EQL, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_BSLS,   KC_1, KC_2, KC_3, KC_QUOT, XXXXXXX,
-                                            _______, XXXXXXX, XXXXXXX,         KC_ENT,    KC_0, KC_GRV
+    [L_Num] = split_layout(
+        QK_MAKE, TO(L_Game), XXXXXXX, XXXXXXX, XXXXXXX,                          KC_LBRC,   KC_7, KC_8, KC_9, KC_MINS,
+        KC_LSFT,    KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX,                          KC_RBRC,   KC_4, KC_5, KC_6,  KC_EQL,
+        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          KC_BSLS,   KC_1, KC_2, KC_3, KC_QUOT,
+                                      _______, XXXXXXX, XXXXXXX,         KC_ENT,    KC_0, KC_GRV
     ),
 
      /* Mouse
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │MKE│   │   │   │   │       │   │   │   │   │   │
+      * │MKE│Gam│   │   │   │       │   │   │   │   │   │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │Sft│GUI│Alt│Ctl│   │       │ ← │ ↓ │ ↑ │ → │   │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
@@ -71,16 +86,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤Hld│   │Rgt├───┘
       *                   └───┘   └───┘
       */
-    [L_Mouse] = LAYOUT_split_3x6_3(
-        XXXXXXX, QK_MAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX,                           MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX, XXXXXXX,
-                                            XXXXXXX, _______, XXXXXXX,         MS_BTN3, MS_BTN1, MS_BTN2
+    [L_Mouse] = split_layout(
+        QK_MAKE, TO(L_Game), XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LSFT,    KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX,                           MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, XXXXXXX,
+        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX,
+                                      XXXXXXX, _______, XXXXXXX,         MS_BTN3, MS_BTN1, MS_BTN2
     ),
 
      /* Navigation
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │MKE│   │   │   │   │       │   │   │   │   │   │
+      * │MKE│Gam│   │   │   │       │   │   │   │   │   │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │Sft│GUI│Alt│Ctl│   │       │ ← │ ↓ │ ↑ │ → │   │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
@@ -92,16 +107,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   │   │   ├───┘
       *                   └───┘   └───┘
       */
-    [L_Nav] = LAYOUT_split_3x6_3(
-        XXXXXXX, QK_MAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX,                           KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
-                                            XXXXXXX, XXXXXXX, _______,         XXXXXXX, XXXXXXX, XXXXXXX
+    [L_Nav] = split_layout(
+        QK_MAKE, TO(L_Game), XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LSFT,    KC_LGUI, KC_LALT, KC_LCTL, XXXXXXX,                           KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX,
+        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                           KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX,
+                                      XXXXXXX, XXXXXXX, _______,         XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
      /* Function
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │F1 │F2 │F3 │F4 │ Å │       │   │   │   │   │MKE│
+      * │F1 │F2 │F3 │F4 │ Å │       │   │   │   │Gam│MKE│
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │F5 │F6 │F7 │F8 │ Ä │       │   │Ctl│Alt│GUI│Sft│
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
@@ -113,16 +128,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   │   │   ├───┘
       *                   └───┘   └───┘
       */
-    [L_Fun] = LAYOUT_split_3x6_3(
-        XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4, US_ARNG,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_MAKE, XXXXXXX,
-        XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8, US_ADIA,                            XXXXXXX, KC_RCTL, KC_RALT, KC_RGUI, KC_RSFT, XXXXXXX,
-        XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12, US_ODIA,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                            XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, _______
+    [L_Fun] = split_layout(
+          KC_F1,   KC_F2,   KC_F3,   KC_F4, US_ARNG,                            XXXXXXX, XXXXXXX, XXXXXXX, TO(L_Game), QK_MAKE,
+          KC_F5,   KC_F6,   KC_F7,   KC_F8, US_ADIA,                            XXXXXXX, KC_RCTL, KC_RALT,    KC_RGUI, KC_RSFT,
+          KC_F9,  KC_F10,  KC_F11,  KC_F12, US_ODIA,                            XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
+                                   XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, _______
     ),
 
      /* Meida
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │   │   │   │   │   │       │   │   │   │   │MKE│
+      * │   │   │   │   │   │       │   │   │   │Gam│MKE│
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │   │Prv│Ply│Nxt│   │       │   │Ctl│Alt│GUI│Sft│
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
@@ -134,11 +149,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   │   │Hld├───┘
       *                   └───┘   └───┘
       */
-    [L_Media] = LAYOUT_split_3x6_3(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_MAKE, XXXXXXX,
-        XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                            XXXXXXX, KC_RCTL, KC_RALT, KC_RGUI, KC_RSFT, XXXXXXX,
-        XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                            XXXXXXX, XXXXXXX, XXXXXXX,          _______, XXXXXXX, XXXXXXX
+    [L_Media] = split_layout(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, TO(L_Game), QK_MAKE,
+        XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                            XXXXXXX, KC_RCTL, KC_RALT,    KC_RGUI, KC_RSFT,
+        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
+                                   XXXXXXX, XXXXXXX, XXXXXXX,          _______, XXXXXXX, XXXXXXX
+    ),
+
+     /* Game
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │Esc│ Q │ W │ E │ R │       │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │Sft│ A │ S │ D │ F │       │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │Ctl│ Z │ X │ C │ V │       │   │   │   │   │   │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *           ┌───┐                   ┌───┐
+      *           │Tab├───┐           ┌───┤   │
+      *           └───┤   ├───┐   ┌───┤   ├───┘
+      *               └───┤Ctl│   │Nrm├───┘
+      *                   └───┘   └───┘
+      */
+    [L_Game] = split_layout(
+         KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,                                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,                                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                    KC_TAB,  KC_SPC, LT(L_GameSnd, KC_LCTL),          TG(L_Game), XXXXXXX, XXXXXXX
+    ),
+
+     /* Game second
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │   │ 7 │ 8 │ 9 │   │       │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │   │ 4 │ 5 │ 6 │   │       │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │   │ 1 │ 2 │ 3 │   │       │   │   │   │   │   │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *           ┌───┐                   ┌───┐
+      *           │   ├───┐           ┌───┤   │
+      *           └───┤   ├───┐   ┌───┤   ├───┘
+      *               └───┤Hld│   │   ├───┘
+      *                   └───┘   └───┘
+      */
+    [L_GameSnd] = split_layout(
+        XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,    KC_4,    KC_5,    KC_6, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                   XXXXXXX, XXXXXXX, _______,          XXXXXXX, XXXXXXX, XXXXXXX
     ),
 };
 
